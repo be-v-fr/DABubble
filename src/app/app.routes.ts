b8b2981aa54354a771f2189dc6d062bb48cf9830
DennisBilowodskyj <d.bilowodskyi@gmail.com>
2024-06-20 23:51:25 +0200

    profile card changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { MainComponent } from './home/main/main.component';
 import { ImpressComponent } from './home/legal/impress/impress.component';
 import { PrivacypolicyComponent } from './home/legal/privacypolicy/privacypolicy.component';
+import { UserProfileCardComponent } from './user-profile-card/user-profile-card.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -11,4 +12,6 @@ export const routes: Routes = [
     {path: 'main', component: MainComponent},
     {path: 'impress', component: ImpressComponent},
     {path: 'privacypolicy', component: PrivacypolicyComponent},
+    {path: 'userProfile', component: UserProfileCardComponent},
+
 ];
--- src/app/user-profile-card/user-profile-card.component.ts
+++ src/app/user-profile-card/user-profile-card.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-user-profile-card',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './user-profile-card.component.html',
   styleUrl: './user-profile-card.component.scss'
 })
