ae9bef561a5dda60b74dad502ac1559381f0b692
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-06-20 14:02:24 +0200

    login component hard-coded

---
+++ src/app/auth/legal-footer/legal-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './legal-footer.component.html',
+  styleUrl: './legal-footer.component.scss'
+})
+export class LegalFooterComponent {
+
+}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { LegalFooterComponent } from '../legal-footer/legal-footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LegalFooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  // ToDo:
+  // - General Functionality
+  //
+  // Details:
+  // - On Input, both text color and icon color should turn to black
+  // - Click on form field surrounding input should focus input
+  // - Password visibility options
+  // - DABubble Logo should be clickable
 }
