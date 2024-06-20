9e8664a591f867bd138e4969e0fb9b1ac605da17
Alexander Leitermann <alexander@leitermann-home.de>
2024-06-20 14:46:31 +0200

    alex first commit, add path legal includs impress and privacypolicy components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { MainComponent } from './home/main/main.component';
+import { ImpressComponent } from './home/legal/impress/impress.component';
+import { PrivacypolicyComponent } from './home/legal/privacypolicy/privacypolicy.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'main', component: MainComponent}
+    {path: 'main', component: MainComponent},
+    {path: 'impress', component: ImpressComponent},
+    {path: 'privacypolicy', component: PrivacypolicyComponent},
 ];
