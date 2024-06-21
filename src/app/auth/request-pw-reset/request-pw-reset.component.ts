cb0249c9c86d4bfea67b7e5d07551c6bc0903ded
Bengt Früchtenicht <befru000@gmail.com>
2024-06-21 13:55:41 +0200

    password reset hard-coded

--- src/app/auth/request-pw-reset/request-pw-reset.component.ts
+++ src/app/auth/request-pw-reset/request-pw-reset.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LegalFooterComponent } from '../legal-footer/legal-footer.component';
 
 @Component({
   selector: 'app-request-pw-reset',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LegalFooterComponent],
   templateUrl: './request-pw-reset.component.html',
   styleUrl: './request-pw-reset.component.scss'
 })
--- src/app/auth/reset-pw/reset-pw.component.ts
+++ src/app/auth/reset-pw/reset-pw.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LegalFooterComponent } from '../legal-footer/legal-footer.component';
 
 @Component({
   selector: 'app-reset-pw',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LegalFooterComponent],
   templateUrl: './reset-pw.component.html',
   styleUrl: './reset-pw.component.scss'
 })
