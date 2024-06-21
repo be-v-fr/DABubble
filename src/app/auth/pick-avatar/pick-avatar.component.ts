96ba2d343a3f6155173fdfd05533e473bc854b06
Bengt Früchtenicht <befru000@gmail.com>
2024-06-21 11:30:59 +0200

    avatar picker hard-coded

--- src/app/auth/pick-avatar/pick-avatar.component.ts
+++ src/app/auth/pick-avatar/pick-avatar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LegalFooterComponent } from '../legal-footer/legal-footer.component';
 
 @Component({
   selector: 'app-pick-avatar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LegalFooterComponent],
   templateUrl: './pick-avatar.component.html',
   styleUrl: './pick-avatar.component.scss'
 })
 export class PickAvatarComponent {
-
+  // ToDo:
+  // - Avatar Picker
 }
