40f2b4a8f32f72eaac13350c8b8d2560e1bb5fec
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-06-22 17:56:59 +0200

    create message-item component

---
+++ src/app/components/message-item/message-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TimeSeparatorComponent } from '../time-separator/time-separator.component';
+
+@Component({
+  selector: 'app-message-item',
+  standalone: true,
+  imports: [TimeSeparatorComponent],
+  templateUrl: './message-item.component.html',
+  styleUrl: './message-item.component.scss'
+})
+export class MessageItemComponent {
+
+}
--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 
 import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
-import { TimeSeparatorComponent } from './time-separator/time-separator.component';
+import { TimeSeparatorComponent } from '../../components/time-separator/time-separator.component';
 import { CommonModule } from '@angular/common';
 import { MessageBoxComponent } from '../../components/message-box/message-box.component';
+import { MessageItemComponent } from '../../components/message-item/message-item.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, PickerComponent, TimeSeparatorComponent, MessageBoxComponent],
+  imports: [CommonModule, PickerComponent, MessageItemComponent, MessageBoxComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
