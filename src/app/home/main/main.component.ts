61002ff3339cf81ba2133c1d363d6c9352b638e0
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-06-22 12:18:32 +0200

    create message-box component

--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -4,11 +4,12 @@ import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 import { TimeSeparatorComponent } from './time-separator/time-separator.component';
 import { CommonModule } from '@angular/common';
+import { MessageBoxComponent } from '../../components/message-box/message-box.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, PickerComponent, TimeSeparatorComponent],
+  imports: [CommonModule, PickerComponent, TimeSeparatorComponent, MessageBoxComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
