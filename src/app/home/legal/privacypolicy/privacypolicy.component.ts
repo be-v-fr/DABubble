fb033fd4abaf8ab31e88eca6bfd1ff250becd0e4
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-06-30 17:16:00 +0200

    auth components further details

--- src/app/home/legal/privacypolicy/privacypolicy.component.ts
+++ src/app/home/legal/privacypolicy/privacypolicy.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-privacypolicy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './privacypolicy.component.html',
   styleUrl: './privacypolicy.component.scss'
 })
-export class PrivacypolicyComponent {
+export class PrivacypolicyComponent implements OnInit {
+  hideBackBtn: boolean = false;
 
   constructor(
     private location: Location,
+    private route: ActivatedRoute
   ) {}
+
+  ngOnInit(): void {
+      this.route.queryParams.subscribe(params => {
+        this.hideBackBtn = (params['back'] === 'false');
+      });
+  }
 
   historyBack() {
     this.location.back();
