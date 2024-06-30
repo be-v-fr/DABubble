bfbba770fa7a28761d6122a34a98703991faf6a4
DennisBilowodskyj <d.bilowodskyi@gmail.com>
2024-06-30 12:46:31 +0200

    edit channel finish

--- src/app/edit-channel/edit-channel.component.ts
+++ src/app/edit-channel/edit-channel.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-channel',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './edit-channel.component.html',
-  styleUrl: './edit-channel.component.scss'
+  styleUrl: './edit-channel.component.scss',
 })
 export class EditChannelComponent {
+  editMode: boolean = false;
+  editDescriptionMode: boolean = false;
+  editName: boolean = false;
+  channelName: string = 'Entwicklerteam';
 
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+  }
+
+  saveChanges() {
+    this.editMode = false;
+  }
+
+  toggleEditDescriptionMode() {
+    this.editDescriptionMode = !this.editDescriptionMode;
+  }
+  toggleEditName() {
+    this.editName = !this.editName;
+  }
+
+  saveNameChanges() {
+    this.editName = false;
+  }
+
+  saveDescriptionChanges() {
+    this.editDescriptionMode = false;
+  }
 }
