af5692853ef14126875b5950869db76bba18d7f6
DennisBilowodskyj <d.bilowodskyi@gmail.com>
2024-07-09 18:20:22 +0200

    add user from addMember to memberList

--- src/app/add-members/add-members.component.ts
+++ src/app/add-members/add-members.component.ts
@@ -3,24 +3,25 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../models/user.class';
 import { ActivityService } from '../../services/activity.service';
-import { MemberListComponent } from '../member-list/member-list.component';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-members',
   standalone: true,
   templateUrl: './add-members.component.html',
-  styleUrl: './add-members.component.scss',
-  imports: [CommonModule, FormsModule, MemberListComponent],
+  styleUrls: ['./add-members.component.scss'],
+  imports: [CommonModule, FormsModule],
 })
 export class AddMembersComponent implements OnInit {
   selection: string | null = null;
   filteredUsers: User[] = [];
-  selectedUsers: User[] = [];
-  showUserList: boolean = false;
   selectedUser: User | null = null;
-  activeUsers: User[] = [];
-  constructor(private activityService: ActivityService,private dialogRef: MatDialogRef<any>) {}
+  showUserList: boolean = false;
+
+  constructor(
+    private activityService: ActivityService,
+    private dialogRef: MatDialogRef<AddMembersComponent>
+  ) {}
 
   ngOnInit(): void {
     this.filteredUsers = this.activityService.getAllUsers();
@@ -50,10 +51,12 @@ export class AddMembersComponent implements OnInit {
   }
 
   addUserToMembers() {
-    
+    if (this.selectedUser) {
+      this.dialogRef.close([this.selectedUser]);
+    }
   }
 
-  closeCard(){
+  closeCard() {
     this.dialogRef.close();
   }
 }
--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,37 +1,45 @@
 import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainChatComponent } from '../components/main-chat/main-chat.component';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { User } from '../../models/user.class';
-import { MatDialog } from '@angular/material/dialog';
 import { AddMembersComponent } from '../add-members/add-members.component';
-import { MatDialogRef } from '@angular/material/dialog';
 import { UserProfileCardComponent } from '../user-profile-card/user-profile-card.component';
+import { UsersService } from '../../services/users.service';
+
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './member-list.component.html',
-  styleUrl: './member-list.component.scss'
+  styleUrls: ['./member-list.component.scss']
 })
-export class MemberListComponent  {
+export class MemberListComponent {
   activeUsers: User[];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { activeUsers: User[] }, private dialog:MatDialog,private dialogRef: MatDialogRef<any>) {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { activeUsers: User[] },
+    private dialog: MatDialog,
+    private dialogRef: MatDialogRef<any>,
+    private usersService: UsersService
+  ) {
     this.activeUsers = data.activeUsers;
   }
 
+  addMember(): void {
+    const dialogRef = this.dialog.open(AddMembersComponent);
 
-
-  addmember():void{
-    this.dialog.open(AddMembersComponent);
+    dialogRef.afterClosed().subscribe((newUsers: User[]) => {
+      if (newUsers && newUsers.length > 0) {
+        this.activeUsers.push(...newUsers);
+      }
+    });
   }
 
   openUserProfile(): void {
     this.dialog.open(UserProfileCardComponent);
   }
 
-  closeCard(){
+  closeCard() {
     this.dialogRef.close();
   }
 }
