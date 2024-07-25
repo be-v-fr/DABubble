0e3bb14a728dea2b1dd06d31bd28adc15e0682da
Bengt Früchtenicht <befru000@gmail.com>
2024-07-25 09:29:21 +0200

    feature: added current user check to edit channel dialog

--- src/app/edit-channel/edit-channel.component.ts
+++ src/app/edit-channel/edit-channel.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Channel } from '../../models/channel.class';
 import { ChannelsService } from '../../services/content/channels.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-edit-channel',
@@ -13,19 +14,37 @@ import { ChannelsService } from '../../services/content/channels.service';
   styleUrls: ['./edit-channel.component.scss'],
 })
 export class EditChannelComponent {
+  userIsAuthor: boolean;
   editMode: boolean = false;
   editDescriptionMode: boolean = false;
   editName: boolean = false;
-  channelName = '';
-  channelDescription = '';
+  channelName: string = '';
+  channelDescription: string = '';
+  channelAuthorName: string = '';
 
   constructor(
     private dialogRef: MatDialogRef<EditChannelComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Channel,
-    private channelService: ChannelsService
+    private channelService: ChannelsService,
+    private authService: AuthService
   ) {
     this.channelName = data.name;
     this.channelDescription = data.description;
+    this.channelAuthorName = this.getAuthorName();
+    this.userIsAuthor = (this.data.author_uid === this.authService.getCurrentUid());
+  }
+
+  getAuthorName(): string {
+    if (!this.isTeamChannel()) {
+      const author = this.data.members.find(m => m.uid === this.data.author_uid);
+      return author ? author.name : 'Unbekannter Nutzer';
+    } else {
+      return 'DABubble';
+    }
+  }
+
+  isTeamChannel(): boolean {
+    return this.data.author_uid === '';
   }
 
   toggleEditMode() {
