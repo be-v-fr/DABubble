c1196c744c50676c50eba082f04c7c4728315e8b
Bengt Früchtenicht <befru000@gmail.com>
2024-07-25 13:51:55 +0200

    feature: refined add member channel form

--- src/app/add-members-after-add-channel/add-members-after-add-channel.component.ts
+++ src/app/add-members-after-add-channel/add-members-after-add-channel.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, Inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, Inject, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Channel } from '../../models/channel.class';
 import { ChannelsService } from '../../services/content/channels.service';
@@ -13,17 +13,25 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrl: './add-members-after-add-channel.component.scss'
 })
 export class AddMembersAfterAddChannelComponent {
-  selection = null;
+  selection: 'allMembers' | 'specificPeople' | null = null;
   specificPeople: string = '';
+  @ViewChild('specificPeopleInput', { read: ElementRef }) specificPeopleInput!: ElementRef<HTMLInputElement>;
   private channelsService = inject(ChannelsService);
-
 
   constructor(
     private dialogRef: MatDialogRef<AddMembersAfterAddChannelComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Channel,
-  ) {}
+  ) { }
 
   redirectToChannel() {
     this.channelsService.addChannelToRoute('main-chat', this.data.channel_id);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(this.selection);
+  }
+
+  autofocus() {
+    setTimeout(() => this.specificPeopleInput.nativeElement.focus(), 40);
   }
 }
