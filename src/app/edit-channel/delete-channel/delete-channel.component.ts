96f048ca76a4494039530d89ce572eb2eb0cd8ae
Bengt Früchtenicht <befru000@gmail.com>
2024-07-26 16:58:08 +0200

    feature: added delete channel component and linked to button

---
+++ src/app/edit-channel/delete-channel/delete-channel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-channel',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-channel.component.html',
+  styleUrl: './delete-channel.component.scss'
+})
+export class DeleteChannelComponent {
+
+}
