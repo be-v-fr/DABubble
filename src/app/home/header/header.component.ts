e14d718b1e0da24e5245308042315725b8fc99f3
Bengt Früchtenicht <befru000@gmail.com>
2024-08-05 09:31:08 +0200

    feat: header channels sub

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { LogOutCardComponent } from '../../main-user/log-out-card/log-out-card.component';
 import { MatDialogModule, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
@@ -10,6 +10,10 @@ import { User } from '../../../models/user.class';
 import { AnimationIntroComponent } from '../../animation-intro/animation-intro.component';
 import { AnimationIntroService } from '../../animation-intro/service/animation-intro.service';
 import { FormsModule } from '@angular/forms';
+import { ChannelsService } from '../../../services/content/channels.service';
+import { Channel } from '../../../models/channel.class';
+import { Post } from '../../../models/post.class';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -31,21 +35,66 @@ import { FormsModule } from '@angular/forms';
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
     @Input() mainUser: User = new User;
     @Input() users: User[] = [];
     searchInput: string = '';
+    userChannels: Channel[] = [];
+    searchResultsChannels: Channel[] = [];
+    searchResultsUsers: User[] = [];
+    searchResultsPosts: Post[] = [];
     @ViewChild('searchbar', { read: ElementRef }) searchbar!: ElementRef<HTMLInputElement>;
 
     constructor(
-        public dialogRef: MatDialogRef<LogOutCardComponent>, 
-        public dialog: MatDialog, 
+        public dialogRef: MatDialogRef<LogOutCardComponent>,
+        public dialog: MatDialog,
         private router: Router,
         private authService: AuthService,
         private usersService: UsersService,
         public activityService: ActivityService,
+        private channelsService: ChannelsService,
+        private channelsSub: Subscription,
         public introService: AnimationIntroService
-    ) {}
+    ) { }
+
+    ngOnInit(): void {
+        this.setUserChannels(this.channelsService.channels);
+        this.channelsSub = this.subChannels();
+    }
+
+    ngOnDestroy(): void {
+        this.channelsSub.unsubscribe();
+    }
+
+    subChannels(): Subscription {
+        return this.channelsService.channels$.subscribe((channels: Channel[]) => this.setUserChannels(channels));
+    }
+
+    setUserChannels(allChannels: Channel[]): void {
+        this.userChannels = allChannels.filter(c => c.members.some(m => m.uid === this.mainUser.uid));
+    }
+
+    search(): void {
+        if (this.searchInput.length > 0) {
+            this.searchChannels();
+            this.searchUsers();
+            this.searchPosts();
+        } else {
+            this.clearSearch();
+        }
+    }
+
+    searchChannels(): void {
+
+    }
+
+    searchUsers(): void {
+
+    }
+
+    searchPosts(): void {
+
+    }
 
     onCloseSearchClick(): void {
         this.clearSearch();
@@ -54,6 +103,9 @@ export class HeaderComponent {
 
     clearSearch(): void {
         this.searchInput = '';
+        this.searchResultsChannels = [];
+        this.searchResultsUsers = [];
+        this.searchResultsPosts = [];
     }
 
     autofocusSearch(): void {
