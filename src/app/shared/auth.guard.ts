de8e2375952956bdb6b152a11d019aac6f9310ee
Bengt Früchtenicht <befru000@gmail.com>
2024-09-26 17:44:42 +0200

    docs: shared folder

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,6 +1,16 @@
 import { CanActivateFn } from '@angular/router';
 import { Router } from '@angular/router';
 
+
+/**
+ * Custom route guard that handles authentication and specific query parameters for password reset or email verification.
+ * It intercepts routes, checks for specific query parameters, and navigates to appropriate routes accordingly
+ * to prevent the standard route direction to the login form or home / new message view.
+ * 
+ * @param route - The activated route snapshot containing route information, including query parameters.
+ * @param state - The current router state.
+ * @returns A boolean indicating whether to allow or prevent navigation to the route.
+ */
 export const authGuard: CanActivateFn = (route, state) => {
   const router = new Router();
   const mode = route.queryParams['mode'];
