3e2ab28e4474fff1e9e9e9e8ed4b8084c513fe1c
Bengt Früchtenicht <befru000@gmail.com>
2024-09-26 13:13:46 +0200

    docs: reaction service

--- src/services/content/reaction.service.ts
+++ src/services/content/reaction.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject } from 'rxjs';
 import { User } from '../../models/user.class';
 import { Post } from '../../models/post.class';
 import { ChannelsService } from './channels.service';
+import { Reaction } from '../../models/reaction.class';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,7 @@ import { ChannelsService } from './channels.service';
 export class ReactionService {
   private _reactionsPicker$ = new BehaviorSubject<boolean>(false);
   reactionsPicker$ = this._reactionsPicker$.asObservable();
-
   reactionToAdded$ = new BehaviorSubject<string>('');
-
   reactionToMessage = false;
   reactionToEditMessage = false;
   addReactionInThread = false;
@@ -22,53 +21,42 @@ export class ReactionService {
     private channelsService: ChannelsService
   ) { }
 
+
+  /**
+   * Sets the reaction to be added and updates the corresponding BehaviorSubject.
+   * @param {string} reaction - The reaction (emoji) to be added.
+   */
   setReaction(reaction: string): void {
     this.reactionToAdded$.next(reaction);
   }
 
+
+  /**
+   * Toggles the state of the reactions picker between open and closed.
+   */
   toggleReactionsPicker() {
     this._reactionsPicker$.next(!this._reactionsPicker$.value);
   }
 
-  async addReaction(event: any, user: User): Promise<void> {
-    if (!user) {
-      console.error('Current user is not defined');
-      return;
-    }
 
+  /**
+   * Adds or removes a reaction from a post. If the user has already reacted with the same emoji, 
+   * the reaction is removed. Otherwise, the reaction is added.
+   * @param {any} event - The event that contains the selected emoji.
+   * @param {User} user - The user who is adding or removing the reaction.
+   */
+  async addReaction(event: any, user: User): Promise<void> {
     if (!this.currentPost) {
       console.error('Current post is not defined');
       return;
     }
-
-    const existingReaction = this.currentPost.reactions.find(
-      r => r.emoji === event.emoji.native && r.user.uid === user.uid
-    );
-
     try {
-      if (existingReaction) {
-        await this.channelsService.deleteReactionFromPost(
-          this.currentPost.channel_id,
-          this.currentPost.post_id,
-          user.uid,
-          event.emoji.native
-        );
-      } else {
-        await this.channelsService.addReactionToPost(
-          this.currentPost.channel_id,
-          this.currentPost.post_id,
-          user,
-          event.emoji.native
-        );
-      }
-
+      await this.toggleReaction(event, user);
       const posts = this.channelsService.getChannelThreadPosts(
         this.currentPost.channel_id,
         this.currentPost.post_id
       );
-
       this.currentPost = posts?.find(p => p.post_id === this.currentPost.post_id) || this.currentPost;
-
     } catch (error) {
       console.error('Error handling reaction for post:', error);
     } finally {
@@ -76,4 +64,42 @@ export class ReactionService {
     }
   }
 
+
+  /**
+   * Retrieves a reaction from the current post property, if it exists.
+   * @param {any} event - The event that contains the selected emoji.
+   * @param {User} user - The user who is adding or removing the reaction.
+   * @returns {Reaction | undefined} - Search result.
+   */
+  findReaction(event: any, user: User): Reaction | undefined {
+    return this.currentPost.reactions.find(r =>
+      r.emoji === event.emoji.native && r.user.uid === user.uid
+    );
+  }
+
+
+  /**
+   * Adds or removes a reaction from a post in backend, depending an whether
+   * or not the reaction already exists.
+   * @param {any} event - The event that contains the selected emoji.
+   * @param {User} user - The user who is adding or removing the reaction.
+   */
+  async toggleReaction(event: any, user: User) {
+    const existingReaction = this.findReaction(event, user);
+    if (existingReaction) {
+      await this.channelsService.deleteReactionFromPost(
+        this.currentPost.channel_id,
+        this.currentPost.post_id,
+        user.uid,
+        event.emoji.native
+      );
+    } else {
+      await this.channelsService.addReactionToPost(
+        this.currentPost.channel_id,
+        this.currentPost.post_id,
+        user,
+        event.emoji.native
+      );
+    }
+  }
 }
