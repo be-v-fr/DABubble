2093dcedc4fa9f4a31f91f2cc25d45363761bb78
Bengt Früchtenicht <befru000@gmail.com>
2024-09-28 11:49:42 +0200

    docs: delete channel

--- src/app/components/edit-channel/delete-channel/delete-channel.component.ts
+++ src/app/components/edit-channel/delete-channel/delete-channel.component.ts
@@ -3,6 +3,11 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ChannelsService } from '../../../../services/content/channels.service';
 import { Router } from '@angular/router';
 
+
+/**
+ * Component for handling the deletion of a channel.
+ * It opens as a dialog and allows the user to confirm the deletion of a channel.
+ */
 @Component({
   selector: 'app-delete-channel',
   standalone: true,
@@ -13,6 +18,15 @@ import { Router } from '@angular/router';
 export class DeleteChannelComponent {
   channel_id: string | null = null;
 
+
+  /**
+   * Constructor for DeleteChannelComponent.
+   * 
+   * @param dialogRef - Reference to the dialog instance for managing the dialog's state.
+   * @param data - Injected data, including the channel ID and other relevant details.
+   * @param channelsService - Service for managing channels, including retrieving and deleting them.
+   * @param router - Angular Router for navigation after the channel is deleted.
+   */
   constructor(
     private dialogRef: MatDialogRef<DeleteChannelComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -22,10 +36,20 @@ export class DeleteChannelComponent {
     this.channel_id = data.channel_id;
   }
 
+
+  /**
+   * Closes the delete channel dialog.
+   */
   close() {
     this.dialogRef.close();
   }
 
+
+  /**
+   * Deletes the channel if the channel ID is available.
+   * It first retrieves the channel, then deletes it using the ChannelsService.
+   * After deletion, it navigates the user to a default route and closes the relevant dialogs.
+   */
   async deleteChannel() {
     if (this.channel_id) {
       const channel = await this.channelsService.getChannel(this.channel_id);
