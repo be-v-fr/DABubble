d353ea15cc82a60270cfcd65381918827531d39e
Bengt Früchtenicht <befru000@gmail.com>
2024-10-18 09:22:00 +0200

    feat: new firebase config and config hidden trough gitignore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,20 +7,14 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getStorage, provideStorage } from '@angular/fire/storage'
+import { firebaseConfig } from './firebase.config';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp({
-      apiKey: "AIzaSyDuQGRRfGVVjVBJRhRprhg3bG61pHevMvM",
-      authDomain: "dabubble-257.firebaseapp.com",
-      projectId: "dabubble-257",
-      storageBucket: "dabubble-257.appspot.com",
-      messagingSenderId: "1048437261939",
-      appId: "1:1048437261939:web:6545ca4790290b7b660c06"
-    })),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
     provideAnimations(),
